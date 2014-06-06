8e65fea5bf5a9d320365469c6eec1bad191c2b48
Victor Almeida <viktorsnt@gmail.com>
2014-06-06 19:12:52 -0300

    initial setup

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+    $(document).ready(function(){
+      $('img').addClass("img-responsive");
+    });
+}(jQuery));
